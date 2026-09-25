aaa3efbbcbe1024a46921d9f573c905fef319e49
Romain L <lenson.romain@gmail.com>
2026-09-25 12:36:18 +0200

    Change ui scale

---
+++ Assets/_Scripts/DoubleScreen.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DoubleScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Screen.SetResolution(3840, 1080, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
